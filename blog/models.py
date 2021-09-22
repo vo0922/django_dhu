fff49f3c11d55cee9069a313317e3f1e65b7892b
vo0922 <71069665+vo0922@users.noreply.github.com>
2021-09-23 00:18:39 +0900

    blog상세페이지
    0923

--- blog/models.py
+++ blog/models.py
@@ -4,5 +4,13 @@ class Post(models.Model):
     title = models.CharField(max_length=30)
     content = models.TextField()
 
-    created_at = models.DateTimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    # author: 추후 작성 예정
+
+    def __str__(self):
+        return f'[{self.pk}]{self.title}'
+
+    def get_absolute_url(self):
+        return f'/blog/{self.pk}/'
 # Create your models here.
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Post
 
+class PostList(ListView):
+    model = Post
+    ordering = '-pk'
+    template_name = 'blog/index.html'
+
+class PostDetail(DetailView):
+    model = Post
+
+#def index(request):
+ #   posts = Post.objects.all().order_by('-pk')
+ #
+ #   return render(
+ #       request,
+ #       'blog/index.html',
+ #       {
+ #           'posts' : posts,
+ #       }
+ #   )
+
+def single_post_page(request, pk):
+    post = Post.objects.get(pk=pk)
+
+    return render(
+        request,
+        'blog/post_detail.html',
+        {
+            'post' : post,
+        }
+    )
 # Create your views here.
