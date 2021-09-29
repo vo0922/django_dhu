562b5459d42924d531d2cbca9d48174ffba843c9
vo0922 <71069665+vo0922@users.noreply.github.com>
2021-09-30 01:26:41 +0900

    media manage
    2021-09-29

--- blog/models.py
+++ blog/models.py
@@ -4,6 +4,9 @@ class Post(models.Model):
     title = models.CharField(max_length=30)
     content = models.TextField()
 
+    head_image = models.ImageField(upload_to='blog/images/%y/%m/%d/', blank=True)
+    file_upload = models.FileField(upload_to='blog/files/%y/%m/%d', blank=True)
+
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     # author: 추후 작성 예정
